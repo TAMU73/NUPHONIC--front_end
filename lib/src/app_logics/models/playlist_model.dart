8a197c1be38573a229788f1d49f387a59826f419
TAMU73 <47109205+TAMU73@users.noreply.github.com>
2021-02-24 16:37:25 +0545

    Playlist screen in library completed

--- lib/src/app_logics/models/playlist_model.dart
+++ lib/src/app_logics/models/playlist_model.dart
@@ -1,11 +1,35 @@
+import 'package:nuphonic_front_end/src/app_logics/models/song_model.dart';
+
 class PlaylistModel {
   final String playlistId;
   final String playlistName;
-  final List<String> playlistSongs;
+  final List playlistSongs;
+  List<SongModel> playlistSongModel;
+  final String userID;
 
   PlaylistModel({
     this.playlistId,
     this.playlistName,
     this.playlistSongs,
+    this.userID,
+    this.playlistSongModel,
   });
+
+  factory PlaylistModel.fromJson(Map<String, dynamic> data) {
+    return PlaylistModel(
+      playlistSongs: data['playlist_songs'],
+      playlistName: data['playlist_name'],
+      playlistId: data['_id'],
+      userID: data['user_id'],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['playlist_songs'] = this.playlistSongs;
+    data['_id'] = this.playlistId;
+    data['playlist_name'] = this.playlistName;
+    data['user_id'] = this.userID;
+    return data;
+  }
 }
--- lib/src/views/screens/library/favourites/playlists.dart
+++ lib/src/views/screens/library/favourites/playlists.dart
@@ -1,38 +1,121 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:nuphonic_front_end/src/app_logics/models/playlist_model.dart';
+import 'package:nuphonic_front_end/src/app_logics/models/song_model.dart';
+import 'package:nuphonic_front_end/src/app_logics/services/api_services/playlist_services.dart';
+import 'package:nuphonic_front_end/src/app_logics/services/api_services/song_service.dart';
+import 'package:nuphonic_front_end/src/app_logics/services/shared_pref_services/shared_pref_service.dart';
 import 'package:nuphonic_front_end/src/views/reusable_widgets/custom_bottom_sheet.dart';
 import 'package:nuphonic_front_end/src/views/reusable_widgets/custom_error.dart';
+import 'package:nuphonic_front_end/src/views/reusable_widgets/custom_snackbar.dart';
 import 'package:nuphonic_front_end/src/views/utils/consts.dart';
 import 'package:sliding_up_panel/sliding_up_panel.dart';
 
-class Playlists extends StatelessWidget {
+class Playlists extends StatefulWidget {
+  @override
+  _PlaylistsState createState() => _PlaylistsState();
+}
+
+class _PlaylistsState extends State<Playlists> {
   PanelController _panelController = PanelController();
   TextEditingController _textEditingController = TextEditingController();
+  PlaylistServices _playlistServices = PlaylistServices();
+  SharedPrefService _sharedPrefService = SharedPrefService();
+  CustomSnackBar _customSnackBar = CustomSnackBar();
+  SongService _song = SongService();
 
-  List<PlaylistModel> _allPlaylists = [
-    PlaylistModel(
-      playlistName: 'Chill',
-      playlistSongs: ['asdsadlasd', 'djfkahjkhas'],
-    ),
-    PlaylistModel(
-      playlistName: 'Chill',
-      playlistSongs: ['asdsadlasd', 'djfkahjkhas'],
-    ),
-    PlaylistModel(
-      playlistName: 'Chill',
-      playlistSongs: ['asdsadlasd', 'djfkahjkhas', ''],
-    ),
-  ];
+  List<PlaylistModel> _allPlaylists = [];
+
+  String newPlaylistName = "";
+  bool isLoading = false;
+  bool isButtonLoading = false;
+
+  Future<List<SongModel>> getSongModel(List playlistSongs) async {
+    List<SongModel> songList = List<SongModel>();
+    for (var song in playlistSongs) {
+      dynamic result = await _song.getSongDetails(song);
+      songList.add(SongModel.fromJson(result.data['song']));
+    }
+    return songList;
+  }
+
+  Future<void> getUserPlaylist() async {
+    setState(() {
+      isLoading = true;
+    });
+    dynamic userID = await _sharedPrefService.read(id: 'user_id');
+    dynamic result = await _playlistServices.getUserPlaylists(userID);
+    if (result == null) {
+      _customSnackBar.buildSnackBar('Network Error', false);
+    } else {
+      _customSnackBar.buildSnackBar(result.data['msg'], result.data['success']);
+      if (result.data['success']) {
+        dynamic playlistList = result.data['playlists'];
+        _allPlaylists.clear();
+        for (var playlist in playlistList) {
+          setState(() {
+            _allPlaylists.add(PlaylistModel.fromJson(playlist));
+          });
+          for (var playlist in _allPlaylists) {
+            var playlistSongModel = await getSongModel(playlist.playlistSongs);
+            setState(() {
+              playlist.playlistSongModel = playlistSongModel;
+            });
+          }
+        }
+        setState(() {
+          isLoading = false;
+        });
+      }
+    }
+  }
+
+  Future<void> createNewPlaylist() async {
+    setState(() {
+      isButtonLoading = true;
+    });
+    dynamic userID = await _sharedPrefService.read(id: 'user_id');
+    dynamic result =
+        await _playlistServices.createPlaylist(newPlaylistName, userID);
+    setState(() {
+      isButtonLoading = false;
+    });
+    _panelController.close();
+    if (result == null) {
+      _customSnackBar.buildSnackBar('Network Error', false);
+    } else {
+      _customSnackBar.buildSnackBar(result.data['msg'], result.data['success']);
+      if (result.data['success']) {
+        getUserPlaylist();
+      }
+    }
+  }
+
+  Future<void> deletePlaylist(PlaylistModel playlist) async {
+    setState(() {
+      _allPlaylists.remove(playlist);
+    });
+    dynamic result =
+        await _playlistServices.deletePlaylists(playlist.playlistId);
+    if (result == null) {
+      _customSnackBar.buildSnackBar('Network Error', false);
+    } else {
+      _customSnackBar.buildSnackBar(result.data['msg'], result.data['success']);
+    }
+  }
 
   Widget _customButtonSheet() {
     return CustomBottomSheet(
-      onChanged: null,
+      onChanged: (val) {
+        setState(() {
+          newPlaylistName = val;
+        });
+      },
       titleName: 'New Playlist',
       labelName: 'Playlist Name',
       hintName: 'Playlist Name',
-      onPressed: null,
-      isLoading: false,
+      onPressed: newPlaylistName == "" ? null : createNewPlaylist,
+      isLoading: isButtonLoading,
       controller: _panelController,
       buttonName: 'CREATE',
       textController: _textEditingController,
@@ -89,33 +172,24 @@ class Playlists extends StatelessWidget {
     );
   }
 
-  Widget _playlistSongs() {
-    return Row(
-      children: [
-        Flexible(
-            flex: 1,
-            child: Image.network(
-              'https://miro.medium.com/max/3840/1*LPESvqEeQ9V3DAx-6cD6SQ.jpeg',
-              height: 97,
-              fit: BoxFit.cover,
-            )),
-        Flexible(
-            flex: 1,
-            child: Image.network(
-              'https://ca-times.brightspotcdn.com/dims4/default/0b6a1bb/2147483647/strip/true/crop/1920x804+0+0/resize/1486x622!/quality/90/?url=https%3A%2F%2Fcalifornia-times-brightspot.s3.amazonaws.com%2Fdd%2Ffd%2F6078aad94b96a2b9f7919ac74826%2Ffrozen-2-online-use-213-62-56.jpg',
-              height: 97,
-              fit: BoxFit.cover,
-            )),
-        Flexible(
-          flex: 1,
-          child: Image.network(
-            'https://miro.medium.com/max/3840/1*LPESvqEeQ9V3DAx-6cD6SQ.jpeg',
-            height: 97,
-            fit: BoxFit.cover,
-          ),
-        ),
-      ],
-    );
+  Widget _playlistSongs(PlaylistModel playlist) {
+    return playlist.playlistSongs.isEmpty
+        ? Container(color: textFieldColor)
+        : Row(
+            children: playlist.playlistSongModel
+                .map(
+                  (song) => Flexible(
+                    flex: 1,
+                    child: Image.network(
+                      song.songImage,
+                      height: 97,
+                      width:
+                          Size.infinite.width / playlist.playlistSongs.length,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                )
+                .toList());
   }
 
   Widget _gradientEffect() {
@@ -126,7 +200,7 @@ class Playlists extends StatelessWidget {
             end: Alignment.bottomCenter,
             colors: [
               Colors.transparent,
-              backgroundColor.withOpacity(0.7),
+              backgroundColor.withOpacity(0.8),
             ]),
       ),
     );
@@ -175,8 +249,10 @@ class Playlists extends StatelessWidget {
             alignment: Alignment.centerRight,
             children: [
               _imageDummy(),
-              _playlistSongs(),
-              _gradientEffect(),
+              _playlistSongs(playlist),
+              playlist.playlistSongs.isNotEmpty
+                  ? _gradientEffect()
+                  : SizedBox(),
               Positioned(
                 left: 10,
                 bottom: 5,
@@ -188,6 +264,15 @@ class Playlists extends StatelessWidget {
                   ],
                 ),
               ),
+              Positioned(
+                right: 20,
+                child: InkWell(
+                  onTap: () {
+                    deletePlaylist(playlist);
+                  },
+                  child: SvgPicture.asset('assets/icons/delete.svg'),
+                ),
+              )
             ],
           ),
         ),
@@ -196,22 +281,31 @@ class Playlists extends StatelessWidget {
   }
 
   @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getUserPlaylist();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Stack(
       children: [
-        SingleChildScrollView(
-          child: Column(
-            children: [
-              _createNewPlaylist(),
-              SizedBox(
-                height: 20,
+        isLoading
+            ? loading
+            : SingleChildScrollView(
+                child: Column(
+                  children: [
+                    _createNewPlaylist(),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    _allPlaylists.length == 0
+                        ? _showErrorMessage()
+                        : _showPlaylists(),
+                  ],
+                ),
               ),
-              _allPlaylists.length == 0
-                  ? _showErrorMessage()
-                  : _showPlaylists(),
-            ],
-          ),
-        ),
         _customButtonSheet()
       ],
     );
